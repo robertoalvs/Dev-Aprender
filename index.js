84b939c632806495ad2d594b18bf177fc32029cd
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-02-10 14:11:14 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// OBEJETOS
+
+let nome = 'Roberto'
+let sobreNome = 'Alves'
+let idade = 41
+let nasci = 1981
+
+let dados = {
+
+    nome: 'Roberto',
+    sobreNome: 'Alves',
+    idade: 41,
+    nasci: 1981
+
+    
+}
+
+console.log(dados)
